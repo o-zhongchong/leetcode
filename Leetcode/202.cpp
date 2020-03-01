a21f22b33babc022e8345a013381a747d8eb8626
abelzhong <z_chong@126.com>
2020-03-01 16:53:04 +0800

    add file Leetcode/202.cpp

--- Leetcode/202.cpp
+++ Leetcode/202.cpp
@@ -1,26 +1,18 @@
 class Solution {
 public:
     bool isHappy(int n) {
-        unordered_set<int> loop_chk;
-        loop_chk.insert(n);
-        while( n!= 1 )
+        if(4 == n)
+            return false;
+        
+        int sum = 0;
+        while(n)
         {
-            int sum=0;
-            while(n)
-            {
-                int tmp = n%10;
-                n /= 10;
-                sum += tmp * tmp;
-            }
-            n = sum;
-            if( loop_chk.find(n) == loop_chk.end() )
-                loop_chk.insert(n);
-            else
-                break;
+            sum = sum + (n%10)*(n%10);
+            n = n/10;
         }
-        if( n == 1)
-            return 1;
+        if(1==sum)
+            return true;
         else
-            return 0;
+            return isHappy(sum);
     }
 };
