3271f70498203b852885e25dbdd07352d4111e83
zhongchongchong <z_chong@126.com>
2020-08-23 19:49:41 +0800

    modified:   Leetcode/260.cpp

--- Leetcode/260.cpp
+++ Leetcode/260.cpp
@@ -1,23 +1,35 @@
 class Solution {
 public:
     vector<int> singleNumber(vector<int>& nums) {
-        int tmp=0;
-        for(auto i : nums)
-        {   
-            tmp ^= i;    
-        }
-        int mask = tmp & (-tmp);
-        int a = 0, b = 0;
-        for(auto i : nums)
+        int bitflag = 0;
+        int mask = 1;
+        
+        for(auto n : nums)
         {
-            if(mask & i)
-                a ^= i;
-            else
-                b ^= i;
+            bitflag ^= n;
         }
-        vector<int> res;
-        res.push_back(a);
-        res.push_back(b);
-        return res;
+        
+        while( (mask & bitflag) == 0)
+        {
+            mask = mask << 1;
+        }
+        
+        vector<int> ret;
+        ret.push_back(0);
+        ret.push_back(0);
+        
+        for(auto n : nums)
+        {
+            if(n & mask)
+            {
+                ret[0] ^= n;
+            }
+            else
+            {
+                ret[1] ^= n; 
+            }
+        }
+        
+        return ret;
     }
 };
--- README.md
+++ README.md
@@ -49,6 +49,7 @@
 |219|[Contains Duplicate II](https://leetcode.com/problems/contains-duplicate-ii/)|[C++](./Leetcode/219.cpp)|
 |220|[Contains Duplicate III](https://leetcode.com/problems/contains-duplicate-iii/)|[C++](./Leetcode/220.cpp)|
 |242|[Valid Anagram](https://leetcode.com/problems/valid-anagram/)|[C++](./Leetcode/242.cpp)|
+|260|[Single Number III](https://leetcode.com/problems/single-number-iii/)|[C++](./Leetcode/260.cpp)|
 |274|[H-Index](https://leetcode.com/problems/h-index/)|[C++](./Leetcode/274.cpp)|
 |324|[Wiggle Sort II](https://leetcode.com/problems/wiggle-sort-ii/)|[C++](./Leetcode/324.cpp)|
 |338|[Counting Bits](https://leetcode.com/problems/counting-bits/)|[C++](./Leetcode/338.cpp)|
