c7101cf7f24a68723a932cc7fb473f160929e0d3
zhongchongchong <z_chong@126.com>
2020-08-23 17:59:53 +0800

    modified:   Leetcode/137.cpp

--- Leetcode/137.cpp
+++ Leetcode/137.cpp
@@ -1,26 +1,18 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        vector<unsigned char> bitmap(32,0);
-        int res=0;
+        int x1, x2, mask;
+        x1 = x2 = 0;
         
         for( auto i : nums)
         {
-            for(int j=0; j<32; ++j)
-            {
-                bitmap.at(j) += i>>j & 1;
-                bitmap.at(j) %= 3;
-            }
+            x2 ^= (x1 & i);
+            x1 ^= i;
+            mask = ~(x1 & x2);
+            x1 &= mask;
+            x2 &= mask;
         }
         
-        for(int j=0; j<32; ++j)
-        {
-            if( bitmap.at(j) )
-            {
-                res |= 1<<j;
-            }
-        }
-        
-        return res;
+        return x1;
     }
 };
