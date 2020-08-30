7b7a5f34eb333095088070bcc6e114e1c05abe9a
zhongchongchong <z_chong@126.com>
2020-08-31 00:16:54 +0800

    modified:   Leetcode/182.sql

--- Leetcode/182.sql
+++ Leetcode/182.sql
@@ -1,3 +1,4 @@
 # Write your MySQL query statement below
-select distinct p1.Email from Person p1 join Person p2 on p1.Email = p2.Email
-where p1.Id > p2.Id
+select Email from Person
+group by Email
+having count(Email) > 1
