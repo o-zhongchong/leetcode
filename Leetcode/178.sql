eb030d6e381b9e444060103d35d4b515454b3411
zhongchongchong <z_chong@126.com>
2020-08-30 22:25:13 +0800

    modified:   Leetcode/178.sql

--- Leetcode/178.sql
+++ Leetcode/178.sql
@@ -1,3 +1,3 @@
-/* Write your T-SQL query statement below */
-select Score, (select count(distinct Score) from Scores where Score >= s.Score)  Rank
-from Scores s order by Score desc
+# Write your MySQL query statement below
+SELECT s.Score, (SELECT COUNT(DISTINCT Score) FROM Scores WHERE Score >= s.Score) as `Rank`
+FROM Scores s ORDER BY Score DESC;
