26c6c2d9a8fb318f5d4a13ed7817465b7cd7cbe6
zhongchongchong <z_chong@126.com>
2020-10-09 19:52:19 +0800

    modified:   Leetcode/343.cpp

--- Leetcode/343.cpp
+++ Leetcode/343.cpp
@@ -1,18 +1,35 @@
 class Solution {
 public:
-    int integerBreak(int n) {
-        if(n<=1)
-            return n;
-        if(n==2)
-            return 1;
-        if(n==3)
-            return 2;
-        int sum=1;
-        while(n>4)
+    vector<int> dp;
+    
+    Solution()
+    {
+        dp = vector<int>(59,0);
+        dp[1] = dp[2] = 1;
+        
+        for(int i=3; i<59; ++i)
         {
-            sum*=3;
-            n-=3;
+            for(int j=1; j<i; ++j)
+            {
+                if(dp[j] * (i-j) > dp[i])
+                {
+                    dp[i] = dp[j] * (i-j);
+                }
+                
+                if(j * (i-j) > dp[i])
+                {
+                    dp[i] = j * (i-j);
+                }
+            }
         }
-        return sum*n;
+    }
+    
+    int integerBreak(int n) {
+        if(n <=0 || n > 58)
+        {
+            return 0;
+        }
+        
+        return dp[n]; 
     }
 };
