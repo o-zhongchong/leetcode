f8fcb8393be27f5911c3afaed18f6068ece4e767
zhongchongchong <z_chong@126.com>
2020-10-25 13:22:28 +0800

    modified:   Leetcode/59.cpp

--- Leetcode/59.cpp
+++ Leetcode/59.cpp
@@ -1,22 +1,60 @@
 class Solution {
 public:
     vector<vector<int>> generateMatrix(int n) {
-        if(n<=0)
-            return {};
-        vector<vector<int>> res(n, vector<int>(n,0) );
-        int lx=0, ly=0, rx=n, ry=n;
-        int cnt=0, limit=n*n, x=-1, y=-1;
-        while(cnt<limit)
+        if(n <= 0)
         {
-            for(++lx,++x,++y; cnt<limit && y<ry; ++y)
-                res[x][y]=++cnt;
-            for(--ry,++x,--y; cnt<limit && x<rx; ++x)
-                res[x][y]=++cnt;
-            for(--x,--y,--rx; cnt<limit && y>=ly; --y)
-                res[x][y]=++cnt;
-            for(++y,--x,++ly;cnt<limit && x>=lx; --x)
-                res[x][y]=++cnt;
+            return vector<vector<int>>();
         }
-        return res;
+        
+        vector<vector<int>> ret(n, vector<int>(n, 0));
+        int up, down, left, right, val;
+        up = left = 0;
+        down = right = n - 1;
+        val = 1;
+        
+        while(true)
+        {
+            for(int j = left; j <= right; ++j)
+            {
+                ret[up][j] = val++;
+            }
+            
+            if(++up > down)
+            {
+                break;
+            }
+            
+            for(int i = up; i <= down; ++i)
+            {
+                ret[i][right] = val++;
+            }
+            
+            if(--right < left)
+            {
+                break;
+            }
+            
+            for(int j = right; j >= left; --j)
+            {
+                ret[down][j] = val++;
+            }
+            
+            if(--down < up)
+            {
+                break;
+            }
+            
+            for(int i = down; i >= up; --i)
+            {
+                ret[i][left] = val++;
+            }
+            
+            if(++left > right)
+            {
+                break;
+            }
+        }
+        
+        return ret;
     }
 };
