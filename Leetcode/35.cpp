fcb6ff1ae1d34cb1bc5b1ade997134d7c803886d
zhongchongchong <z_chong@126.com>
2020-11-07 19:33:11 +0800

    modified:   Leetcode/35.cpp

--- Leetcode/35.cpp
+++ Leetcode/35.cpp
@@ -1,7 +1,27 @@
 class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
-        auto p = lower_bound(nums.begin(), nums.end(), target);
-        return p-nums.begin();
+        int i = 0;
+        int j = nums.size();
+        
+        while(i < j)
+        {
+            int mid = i + (j - i)/2;
+            
+            if(target > nums[mid])
+            {
+                i = mid + 1;
+            }
+            else if(target < nums[mid])
+            {
+                j = mid;
+            }
+            else
+            {
+                return mid;
+            }
+        }
+        
+        return i;
     }
 };
