324feacd1933136a5a50b814ecca92d7fd958db4
zhongchongchong <z_chong@126.com>
2020-11-12 23:21:00 +0800

    modified:   Leetcode/122.cpp

--- Leetcode/122.cpp
+++ Leetcode/122.cpp
@@ -1,12 +1,22 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int profile = 0;
-        for(size_t i=1; i<prices.size(); ++i)
+        if(prices.empty())
         {
-            if(prices.at(i) > prices.at(i-1))
-                profile += prices.at(i) - prices.at(i-1);
+            return 0;
         }
-        return profile;
+        
+        int len = prices.size();
+        int sum = 0;
+        
+        for(int i = 1; i < len; ++i)
+        {
+            if(prices[i] > prices[i-1])
+            {
+                sum += (prices[i] - prices[i-1]);
+            }
+        }
+        
+        return sum;
     }
 };
