a27239e61babcc30ccc715c59187e6139bbec53f
zhongchongchong <z_chong@126.com>
2020-11-14 23:49:37 +0800

    modified:   Leetcode/153.cpp

--- Leetcode/153.cpp
+++ Leetcode/153.cpp
@@ -1,16 +1,28 @@
 class Solution {
 public:
     int findMin(vector<int>& nums) {
-        assert( nums.size()>0 );
-        int left=0, right=nums.size()-1;
-        while( left < right )
+        if(nums.empty())
         {
-            int mid = left + (right - left)/2;
-            if( nums.at(mid) >= nums.at(right) )
-                left = mid+1;
-            else
-                right = mid;
+            return 0;
         }
-        return nums.at(left);
+        
+        int left = 0;
+        int right = nums.size() - 1;
+        
+        while(left < right)
+        {
+            int mid = left + (right - left) / 2;
+            
+            if(nums[mid] < nums[right])
+            {
+                right = mid;
+            }
+            else
+            {
+                left = mid + 1;
+            }
+        }
+        
+        return nums[left];
     }
 };
