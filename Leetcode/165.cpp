b024a6df7529642a41fc5487c12ed4772be36788
zhongchongchong <z_chong@126.com>
2020-12-02 22:03:37 +0800

    modified:   Leetcode/165.cpp

--- Leetcode/165.cpp
+++ Leetcode/165.cpp
@@ -1,44 +1,63 @@
-#include<cstdlib>
-
 class Solution {
 public:
-    int compareVersion(string version1, string version2) {
-        int pos1 = 0, pos2 = 0;
-        int val1 = 0, val2 = 0;
-        while(!version1.empty() || !version2.empty())
+    vector<int> get_version(std::string ver)
+    {
+        vector<int> ret;
+        int len = ver.size();
+        int start = 0;
+        
+        for(int i = 0; i < len; ++i)
         {
-            val1 = val2 = 0;
-            pos1 = version1.find(".");
-            pos2 = version2.find(".");
-
-            if(pos1 != version1.npos)
+            if(ver[i] == '.' && i > start)
             {
-                val1 = atoi(version1.substr(0,pos1).c_str() );
-                version1 = version1.substr(pos1+1);
+                string s = ver.substr(start, i - start);
+                int v = std::stoi(s);
+                ret.push_back(v);
+                start = i + 1;
             }
-            else
-            {
-                val1 = atoi(version1.c_str());
-                version1.clear();
-            }
-
-            if(pos2 != version2.npos)
-            {
-                val2 = atoi(version2.substr(0,pos2).c_str());
-                version2 = version2.substr(pos2+1);
-            }
-            else
-            {
-                val2 = atoi(version2.c_str());
-                version2.clear();
-            }
-
-            if(val1 > val2)
-                return 1;
-            else if(val1 < val2)
-                return -1;
         }
+        
+        if(start != len)
+        {
+            string s = ver.substr(start, len - start);
+            int v = std::stoi(s);
+            ret.push_back(v);
+        }
+        
+        return ret;
+    }
+    
+    int compareVersion(string version1, string version2) {
+        vector<int> ver1 = get_version(version1);
+        vector<int> ver2 = get_version(version2);
+        int len1 = ver1.size();
+        int len2 = ver2.size();
+        
+        for(int i=0, j=0; i<len1 || j<len2; ++i, ++j)
+        {
+            int v1, v2;
+            v1 = v2 = 0;
+            
+            if(i<len1)
+            {
+                v1 = ver1[i];    
+            }
+            
+            if(j<len2)
+            {
+                v2 = ver2[j];
+            }
+            
+            if(v1 > v2)
+            {
+                return 1;
+            }
+            else if(v1 < v2)
+            {
+                return -1;
+            }
+        }
+        
         return 0;
     }
 };
-
