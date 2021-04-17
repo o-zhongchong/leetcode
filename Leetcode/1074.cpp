d01f67ea0e4b6f2bb9b164860c0c2f82f7aaa96e
zhongchongchong <z_chong@126.com>
2021-04-18 00:57:35 +0800

    modified:Leetcode/1074.cpp

--- Leetcode/1074.cpp
+++ Leetcode/1074.cpp
@@ -8,32 +8,29 @@ public:
         
         int m = matrix.size();
         int n = matrix[0].size();
-        vector<vector<int>> sum(m+1, vector<int>(n+1, 0));
         int ret = 0;
         
-        for(int i=1; i<=m; ++i)
+        for(int i=0; i<m; ++i)
         {
-            for(int j=1; j<=n; ++j)
+            for(int j=1; j<n; ++j)
             {
-                sum[i][j] = sum[i-1][j] + sum[i][j-1] - sum[i-1][j-1] + matrix[i-1][j-1];
+                matrix[i][j] += matrix[i][j-1];
             }
         }
         
-        for (int i = 1; i <= m; ++i) 
+        for(int i=0; i<n; ++i)
         {
-            for (int j = 1; j <= n; ++j) 
+            for(int j=i; j<n; ++j)
             {
-                for (int p = 1; p <= i; ++p) 
+                unordered_map<int,int> mymap;
+                mymap[0] = 1;
+                int sum = 0;
+                
+                for(int k=0; k<m; ++k)
                 {
-                    for (int q = 1; q <= j; ++q) 
-                    {
-                        int s = sum[i][j] + sum[p-1][q-1] - sum[i][q-1] - sum[p-1][j];
-                        
-                        if(s == target)
-                        {
-                            ++ret;
-                        }
-                    }
+                    sum += matrix[k][j] - (i > 0 ? matrix[k][i-1] : 0);
+                    ret += mymap[sum - target];
+                    ++mymap[sum];
                 }
             }
         }
