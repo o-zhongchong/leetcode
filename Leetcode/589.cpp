dbeddbf1ec25c5c2564fbdd13fea43f374811291
zhongchongchong <z_chong@126.com>
2021-04-20 23:51:08 +0800

    modified:Leetcode/589.cpp

--- Leetcode/589.cpp
+++ Leetcode/589.cpp
@@ -7,29 +7,42 @@ public:
 
     Node() {}
 
+    Node(int _val) {
+        val = _val;
+    }
+
     Node(int _val, vector<Node*> _children) {
         val = _val;
         children = _children;
     }
 };
 */
+
 class Solution {
 public:
-    vector<int> preorder(Node* root) {
-        vector<int> ret;
-        preorder_traversal(root, ret);
-        return ret;
-    }
-private:
-    int preorder_traversal(Node* root, vector<int> &ret)
+    int dfs(Node* root, vector<int> &ret)
     {
-        if( root == nullptr)
-            return -1;
+        if(root == nullptr)
+        {
+            return 0;
+        }
         
         ret.push_back(root->val);
-        for( auto i: root->children )
-            preorder_traversal(i, ret);
+        
+        for(Node* p : root->children)
+        {
+            if(p != nullptr)
+            {
+                dfs(p, ret);
+            }
+        }
         
         return 0;
+    }
+    
+    vector<int> preorder(Node* root) {
+        vector<int> ret;
+        dfs(root, ret);
+        return ret;
     }
 };
