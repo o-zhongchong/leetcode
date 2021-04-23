e7e2dece15ed4fbd98c8176d141f8b2cd090f899
zhongchongchong <z_chong@126.com>
2021-04-23 22:22:02 +0800

    modified:Leetcode/696.cpp

--- Leetcode/696.cpp
+++ Leetcode/696.cpp
@@ -2,41 +2,24 @@ class Solution {
 public:
     int countBinarySubstrings(string s) {
         int len = s.size();
-        int ret = 0;
-        int index = s.find("01");
+        int ret=0, pre=0, cur=1;
         
-        while(index != s.npos)
+        for(int i=1; i<len; ++i)
         {
-            ++ret;
-            int i= index;
-            int j = index + 1;
-            
-            while(i-1 >= 0 && j+1 < len && s[i-1] == s[i] && s[j+1] == s[j])
+            if(s[i] == s[i-1])
             {
-                --i;
-                ++j;
-                ++ret;
+                ++cur;
+            }
+            else
+            {
+                pre = cur;
+                cur = 1;
             }
             
-            index = s.find("01", index+2);
-        }
-        
-        index = s.find("10");
-        
-        while(index != s.npos)
-        {
-            ++ret;
-            int i= index;
-            int j = index + 1;
-            
-            while(i-1 >= 0 && j+1 < len && s[i-1] == s[i] && s[j+1] == s[j])
+            if(pre >= cur)
             {
-                --i;
-                ++j;
                 ++ret;
             }
-            
-            index = s.find("10", index+2);
         }
         
         return ret;
