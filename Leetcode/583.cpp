a7e7a7c66299d2f1b1cf3b72d1523a9fa84597cd
zhongchongchong <z_chong@126.com>
2021-05-07 18:58:08 +0800

    modified:Leetcode/583.cpp

--- Leetcode/583.cpp
+++ Leetcode/583.cpp
@@ -1,34 +1,34 @@
 class Solution {
 public:
-    int minDistance(string word1, string word2) {
-        int len1 = word1.size();
-        int len2 = word2.size();
-        
-        if(len1 == 0 || len2 == 0)
-        {
-            return abs(len1 - len2);
-        }
-        
+    int getLCS(string &s1, string &s2)
+    {
+        int len1 = s1.size();
+        int len2 = s2.size();
         vector<vector<int>> dp(len1 + 1, vector<int>(len2 + 1, 0));
         
-        for(int i=1; i<=len1; ++i)
+        for(int i=0; i<len1; ++i)
         {
-            for(int j=1; j<=len2; ++j)
+            for(int j=0; j<len2; ++j)
             {
-                if(word1[i-1] == word2[j-1])
+                if(s1[i] == s2[j])
                 {
-                    dp[i][j] = dp[i-1][j-1] + 1;
+                    dp[i+1][j+1] = dp[i][j] + 1;
                 }
                 else
                 {
-                    dp[i][j] = dp[i-1][j-1];
+                    dp[i+1][j+1] = max(dp[i+1][j], dp[i][j+1]);
                 }
-                
-                dp[i][j] = max(dp[i][j], dp[i-1][j]);
-                dp[i][j] = max(dp[i][j], dp[i][j-1]);
             }
         }
         
-        return len1 + len2 - dp[len1][len2] * 2;
+        return dp[len1][len2];
+    }
+    
+    int minDistance(string word1, string word2) {
+        int len1 = word1.size();
+        int len2 = word2.size();
+        int lcs = getLCS(word1, word2);
+        
+        return len1 + len2 - 2 * lcs;
     }
 };
