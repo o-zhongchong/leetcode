cd93fbb8c39387eeb21defe5be5497b16d99d454
zhongchongchong <z_chong@126.com>
2021-05-18 23:48:43 +0800

    modified:Leetcode/609.cpp

--- Leetcode/609.cpp
+++ Leetcode/609.cpp
@@ -1,68 +1,88 @@
 class Solution {
 public:
-    vector<string> split(string& s)
+    vector<string> split(string s, char d)
     {
         vector<string> ret;
         int len = s.size();
         
         for(int i=0, j=0; i<len; ++i)
         {
-            if(s[i] == ' ')
+            if(s[i] == d)
             {
-                string t = s.substr(j,i-j);
-                ret.push_back(t);
+                if(i - j > 0)
+                {
+                    ret.push_back(s.substr(j, i-j));
+                }
+                
                 j = i + 1;
-                continue;
             }
-            
-            if(i == len - 1)
+            else if(s[i] != d && i == len-1)
             {
-                string t = s.substr(j,len-j);
-                ret.push_back(t);
+                if(len - j > 0)
+                {
+                    ret.push_back(s.substr(j, len-j));
+                }
+                
+                j = i + 1;
             }
         }
         
         return ret;
     }
     
-    string split2(string& s)
+    vector<string> getContent(string file)
     {
-        int len = s.size();
-        int i = s.find('(');
-        string ret = s.substr(0,i);
-        s = s.substr(i+1, len-1-(i+1));
-        return ret;
+        string::size_type pos;
+         
+        if( (pos = file.find('(')) != string::npos)
+        {
+            int len = file.size();
+            string filename = file.substr(0, pos);
+            string content = file.substr(pos+1, len-1);
+            return {filename, content};
+        }
+        
+        return {};
+    }
+    
+    void helper(vector<string> &files, unordered_map<string, vector<string>> &dupfiles)
+    {
+        if(files.empty())
+        {
+            return;
+        }
+        
+        string dir = files[0];
+        int len = files.size();
+        
+        for(int i=1; i<len; ++i)
+        {
+            vector<string> file = getContent(files[i]);
+            
+            if(file.size() == 2)
+            {
+                dupfiles[file[1]].push_back(dir + "/" + file[0]);
+            }
+        }
+        
+        return;
     }
     
     vector<vector<string>> findDuplicate(vector<string>& paths) {
-        unordered_map<string,vector<string>> dupfile;
+        unordered_map<string, vector<string>> dupfiles;
         vector<vector<string>> ret;
         
-        for(string s : paths)
+        for(auto &path : paths)
         {
-            vector<string> files = split(s);
-            int len = files.size();
-            
-            for(int i=1; i<len; ++i)
-            {
-                string path = files[0] + "/" + split2(files[i]);
-                
-                if(dupfile.find(files[i]) == dupfile.end())
-                {
-                    dupfile[files[i]] = vector<string>(1, path);
-                }
-                else
-                {
-                    dupfile[files[i]].push_back(path);
-                }
-            }
+            vector<string> files = split(path, ' ');
+            helper(files, dupfiles);
         }
         
-        for(auto &f : dupfile)
+        for(auto &fs : dupfiles)
         {
-            if(f.second.size() > 1)
+            if(fs.second.size() > 1)
             {
-                ret.push_back(f.second);
+                ret.push_back(fs.second);
             }
         }
         
