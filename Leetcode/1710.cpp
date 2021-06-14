ae355fb5053ca36910528e7b8a2709213df55282
zhongchongchong <z_chong@126.com>
2021-06-14 19:16:52 +0800

    modified:Leetcode/1710.cpp

--- Leetcode/1710.cpp
+++ Leetcode/1710.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
-    static bool comp(const vector<int>& a, const vector<int>& b)
+    static bool cmp(const vector<int> &a, const vector<int> &b)
     {
         return a[1] > b[1];
     }
     
     int maximumUnits(vector<vector<int>>& boxTypes, int truckSize) {
-        sort(boxTypes.begin(), boxTypes.end(), comp);
-        int sum = 0;
         int len = boxTypes.size();
+        int ret = 0;
+        sort(boxTypes.begin(), boxTypes.end(), cmp);
         
-        for(int i=0; i<len; ++i)
+        for(int i=0, j=0; i<len && j<truckSize; ++i)
         {
-            int cnt = min(truckSize, boxTypes[i][0]);
-            truckSize -= cnt;
-            sum += cnt * boxTypes[i][1];
+            int c = min(truckSize-j, boxTypes[i][0]);
+            ret += c * boxTypes[i][1];
+            j += c;
         }
         
-        return sum;
+        return ret;
     }
 };
