1073a89451dd6ad0f57c1b76f92db2370cfe1bb6
zhongchongchong <z_chong@126.com>
2021-06-16 21:17:57 +0800

    modified:Leetcode/22.cpp

--- Leetcode/22.cpp
+++ Leetcode/22.cpp
@@ -1,12 +1,6 @@
 class Solution {
 public:
-    vector<string> generateParenthesis(int n) {
-        vector<string> ret;
-        dfs(n, n, "", ret);
-        return ret;
-    }
-    
-    int dfs(int left, int right, string s, vector<string>& ret)
+    int dfs(int left, int right, string s, vector<string> &ret)
     {
         if(left > right)
         {
@@ -16,19 +10,24 @@ public:
         if(left == 0 && right == 0)
         {
             ret.push_back(s);
-            return 0;
         }
         
         if(left > 0)
         {
-            dfs(left-1, right, s + "(", ret);
+            dfs(left-1, right, s+'(', ret);
         }
         
         if(right > 0)
         {
-            dfs(left, right-1, s + ")", ret);
+            dfs(left, right-1, s+')', ret);
         }
         
         return 0;
+    }
+    
+    vector<string> generateParenthesis(int n) {
+        vector<string> ret;
+        dfs(n, n, "", ret);
+        return ret;
     }
 };
