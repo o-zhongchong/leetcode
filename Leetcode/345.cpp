112088a75ad471a2cc5796c7a51155afc649ab6f
zhongchongchong <z_chong@126.com>
2021-08-19 21:46:12 +0800

    modified: Leetcode/345.cpp

--- Leetcode/345.cpp
+++ Leetcode/345.cpp
@@ -1,39 +1,14 @@
 class Solution {
 public:
-    bool isVowel(char c)
-    {
-        if(c == 'a' || c == 'e' || c == 'i' || c == 'o' || c =='u' ||
-           c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U')
-        {
-            return true;
-        }
-        
-        return false;
-    }
-    
     string reverseVowels(string s) {
-        int left = 0, right = s.size() - 1;
-        
-        while(left < right)
-        {
-            while(left < right && !isVowel(s[left]))
-            {
-                ++left;
-            }
-            
-            while(left < right && !isVowel(s[right]))
-            {
-                --right;
-            }
-            
-            if(left < right)
-            {
-                swap(s[left], s[right]);
-                ++left;
-                --right;
-            }
+        int left = 0;
+        int right = s.size() - 1;
+        set<char> vow = {'a','e','i','o','u','A','E','I','O','U'};
+        while(left < right) {
+            while(left < right && !vow.count(s[left])) ++left;
+            while(left < right && !vow.count(s[right])) --right;
+            if(left < right) swap(s[left++], s[right--]);
         }
-        
         return s;
     }
 };
