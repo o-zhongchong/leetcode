0f219ac49f2af59cbaa70dcc9e226987c722553d
zhongchongchong <z_chong@126.com>
2021-08-28 11:46:13 +0800

    modified:Leetcode/1480.cpp

--- Leetcode/1480.cpp
+++ Leetcode/1480.cpp
@@ -2,12 +2,11 @@ class Solution {
 public:
     vector<int> runningSum(vector<int>& nums) {
         int len = nums.size();
-        
-        for(int i=1; i<len; ++i)
-        {
-            nums[i] += nums[i-1];
+        vector<int> ret(len, 0);
+        ret[0] = nums[0];
+        for(int i=1; i<len; ++i) {
+            ret[i] = ret[i-1] + nums[i];
         }
-        
-        return nums;
+        return ret;
     }
 };
