60740457219c5e9e9e6fe3c173ff8f5323390ce4
zhongchongchong <z_chong@126.com>
2021-09-01 21:52:55 +0800

    modified:Leetcode/165.cpp

--- Leetcode/165.cpp
+++ Leetcode/165.cpp
@@ -1,63 +1,41 @@
 class Solution {
 public:
-    vector<int> get_version(std::string ver)
-    {
+    vector<int> getVer(const string &version) {
+        int len = version.size();
         vector<int> ret;
-        int len = ver.size();
-        int start = 0;
-        
-        for(int i = 0; i < len; ++i)
-        {
-            if(ver[i] == '.' && i > start)
-            {
-                string s = ver.substr(start, i - start);
-                int v = std::stoi(s);
-                ret.push_back(v);
-                start = i + 1;
+        for(int i=0,j=0; i<len; ++i) {
+            if(version[i] == '.') {
+                string t = version.substr(j,i-j);
+                if(!t.empty()) {
+                    ret.push_back(stoi(t));
+                }
+                j = i + 1;
+                continue;
+            }
+            if(i == len-1) {
+                string t = version.substr(j,len-j);
+                if(!t.empty()) {
+                    ret.push_back(stoi(t));
+                }
+                j = i + 1;
+                continue;
             }
         }
-        
-        if(start != len)
-        {
-            string s = ver.substr(start, len - start);
-            int v = std::stoi(s);
-            ret.push_back(v);
-        }
-        
         return ret;
     }
-    
     int compareVersion(string version1, string version2) {
-        vector<int> ver1 = get_version(version1);
-        vector<int> ver2 = get_version(version2);
-        int len1 = ver1.size();
-        int len2 = ver2.size();
-        
-        for(int i=0, j=0; i<len1 || j<len2; ++i, ++j)
-        {
+        vector<int> ver1, ver2;
+        ver1 = getVer(version1);
+        ver2 = getVer(version2);
+        int len = max(ver1.size(), ver2.size());
+        for(int i=0; i<len; ++i) {
             int v1, v2;
             v1 = v2 = 0;
-            
-            if(i<len1)
-            {
-                v1 = ver1[i];    
-            }
-            
-            if(j<len2)
-            {
-                v2 = ver2[j];
-            }
-            
-            if(v1 > v2)
-            {
-                return 1;
-            }
-            else if(v1 < v2)
-            {
-                return -1;
-            }
+            if(i < ver1.size()) v1 = ver1[i];
+            if(i < ver2.size()) v2 = ver2[i];
+            if(v1 > v2) return 1;
+            if(v1 < v2) return -1;
         }
-        
         return 0;
     }
 };
