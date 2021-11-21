8eafa8a7c9ac4c39829ba7e8091b805ebe2663fe
o-zhongchong <z_chong@126.com>
2021-11-21 19:35:32 +0800

    feat(106): Construct Binary Tree from Inorder and Postorder Traversal

--- Leetcode/106.cpp
+++ Leetcode/106.cpp
@@ -12,30 +12,24 @@
 class Solution {
 public:
     TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
-        return buildTree(inorder, 0, inorder.size()-1,
-                  postorder, 0, postorder.size()-1);
+        int pos = postorder.size();
+        return helper(inorder, 0, inorder.size(), postorder, pos);
     }
-    
-    TreeNode* buildTree(vector<int>& inorder, int left1, int right1,
-                        vector<int>& postorder, int left2, int right2)
-    {
-        if(left1 > right1 || left2 > right2)
-        {
-            return nullptr;
+private:
+    TreeNode* helper(vector<int>& inorder, int left, int right, vector<int>& postorder, int &pos) {
+        if(pos <= 0 || left >= right) return nullptr;
+        TreeNode* r = new TreeNode(postorder[--pos]);
+        int idx = find(inorder, left, right, r->val);
+        if(idx != -1) {
+            r->right = helper(inorder, idx+1, right, postorder, pos);
+            r->left = helper(inorder, left, idx, postorder, pos);
         }
-        
-        TreeNode* root = new TreeNode(postorder[right2]);
-        int pos = left1;
-        
-        while(pos <= right1 && inorder[pos] != postorder[right2])
-        {
-            ++pos;
+        return r;
+    }
+    int find(vector<int> &nums, int left, int right, int target) {
+        for(int i=left; i<right; ++i) {
+            if(nums[i] == target) return i;
         }
-        
-        root->left = buildTree(inorder, left1, pos-1,
-                               postorder, left2, left2+pos-left1-1);
-        root->right = buildTree(inorder, pos+1, right1,
-                                postorder, left2+pos-left1, right2-1);
-        return root;
+        return -1;
     }
 };
