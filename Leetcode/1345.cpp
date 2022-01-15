a85d72502bc4697e6d1ab80fd33b7cfc998ca7ed
o-zhongchong <z_chong@126.com>
2022-01-15 21:09:03 +0800

    feat(1345): Jump Game IV

--- Leetcode/1345.cpp
+++ Leetcode/1345.cpp
@@ -2,58 +2,37 @@ class Solution {
 public:
     int minJumps(vector<int>& arr) {
         int len = arr.size();
+        unordered_map<int,vector<int>> m;
         vector<bool> visited(len, false);
         queue<int> q({0});
-        unordered_map<int,vector<int>> m;
-        int cnt = 0;
         visited[0] = true;
-        
-        for(int i=0; i<len; ++i)
-        {
+        int step = 0;
+        for(int i=0; i<len; ++i) {
             m[arr[i]].push_back(i);
         }
-        
-        while(!q.empty())
-        {
+        while(!q.empty()) {
             int sz = q.size();
-            
-            while(sz--)
-            {
-                int pos = q.front();
-                q.pop();
-                
-                if(pos == len-1)
-                {
-                    return cnt;
-                }
-                
-                if(pos - 1 >= 0 && !visited[pos-1])
-                {
+            for(int i=0; i<sz; ++i) {
+                int pos = q.front(); q.pop();
+                if(pos == len-1) return step;
+                if(pos-1 >= 0 && !visited[pos-1]) {
                     visited[pos-1] = true;
                     q.push(pos-1);
                 }
-                
-                if(pos + 1 < len && !visited[pos+1])
-                {
+                if(pos+1 < len && !visited[pos+1]) {
                     visited[pos+1] = true;
                     q.push(pos+1);
                 }
-                
-                for(auto j : m[arr[pos]])
-                {
-                    if(j != pos && !visited[j])
-                    {
-                        visited[j] = true;
-                        q.push(j);
+                for(auto next : m[arr[pos]]) {
+                    if(!visited[next]) {
+                        visited[next] = true;
+                        q.push(next);
                     }
                 }
-                
                 m.erase(arr[pos]);
             }
-            
-            ++cnt;
+            ++step;
         }
-        
         return -1;
     }
 };
