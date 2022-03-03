f60e3443c33d9656a2b41a9e1735c6c22caa50b3
o-zhongchong <z_chong@126.com>
2022-03-03 20:29:34 +0800

    feat(413): Arithmetic Slices

--- Leetcode/413.cpp
+++ Leetcode/413.cpp
@@ -1,24 +1,24 @@
 class Solution {
 public:
     int numberOfArithmeticSlices(vector<int>& nums) {
+        if(nums.size() < 3) return 0;
         int len = nums.size();
-        int ret = 0;
-        
-        for(int i=0; i<len; ++i)
-        {
-            for(int j=i+2; j<len; ++j)
-            {
-                if(nums[j] - nums[j-1] == nums[j-1] - nums[j-2])
-                {
-                    ++ret;
+        int ans = 0;
+        for(int i=2,j=0; i<len; ++i) {
+            if(nums[i] - nums[i-1] != nums[i-1] - nums[i-2]) {
+                if(i - j > 2) {
+                    ans += count(i-j-2);
                 }
-                else
-                {
-                    break;
-                }
+                j = i - 1;
+            }
+            if(i == len-1 && i - j + 1 > 2) {
+                ans += count(i-j-1);
             }
         }
-        
-        return ret;
+        return ans;
+    }
+private:
+    int count(int n) {
+        return (1+n)*n/2;
     }
 };
