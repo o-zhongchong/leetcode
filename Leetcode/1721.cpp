11fccf9551b1ec5b6cae892ea6c2bf112a7f8cd6
o-zhongchong <z_chong@126.com>
2022-04-04 12:59:28 +0800

    feat(1721): Swapping Nodes in a Linked List

--- Leetcode/1721.cpp
+++ Leetcode/1721.cpp
@@ -10,56 +10,38 @@
  */
 class Solution {
 public:
-    int length(ListNode* head)
-    {
-        int len = 0;
-        
-        while(head != nullptr)
-        {
-            ++len;
-            head = head->next;
-        }
-        
-        return len;
-    }
-    
-    ListNode* getNode(ListNode* head, int k)
-    {
-        ListNode* p = head;
-        
-        for(int i=1; i<k; ++i)
-        {
-            if(p != nullptr && p->next != nullptr)
-            {
-                p = p->next;
-            }
-            else
-            {
-                return nullptr;
-            }
-        }
-        
-        return p;
-    }
-    
     ListNode* swapNodes(ListNode* head, int k) {
         int len = length(head);
-        
-        if(k > len || head == nullptr)
-        {
+        if(k > len || head == nullptr) {
             return head;
         }
-        
         ListNode* p1 = getNode(head, k);
         ListNode* p2 = getNode(head, len - k + 1);
-        
-        if(p1 != nullptr && p2 != nullptr)
-        {
+        if(p1 != nullptr && p2 != nullptr) {
             int tmp = p1->val;
             p1->val = p2->val;
             p2->val = tmp;
         }
-        
         return head;
+    }
+private:
+    inline int length(ListNode* head) {
+        int len = 0;
+        while(head != nullptr) {
+            ++len;
+            head = head->next;
+        }
+        return len;
+    }
+    ListNode* getNode(ListNode* head, int k) {
+        ListNode* p = head;
+        for(int i=1; i<k; ++i) {
+            if(p != nullptr && p->next != nullptr) {
+                p = p->next;
+            } else {
+                return nullptr;
+            }
+        }
+        return p;
     }
 };
