e3340e715808e928876215484265fa99c4958b99
o-zhongchong <z_chong@126.com>
2022-04-27 20:39:54 +0800

    feat(1202): Smallest String With Swaps

--- Leetcode/1202.cpp
+++ Leetcode/1202.cpp
@@ -1,64 +1,34 @@
 class Solution {
 public:
-    int find(int* ds, int x)
-    {
-        return ds[x] == x ? x : ds[x] = find(ds, ds[x]);
-    }
-    
-    int merge(int* ds, int x, int y)
-    {
-        int fx = find(ds, x);
-        int fy = find(ds, y);
-        
-        if(fx != fy)
-        {
-            ds[fx] = fy;
-            return 1;
-        }
-        
-        return 0;
-    }
-    
     string smallestStringWithSwaps(string s, vector<vector<int>>& pairs) {
         int n = s.size();
-        int* ds = new int[n];
-        vector<string> str(n, "");
-        
-        for(int i=0; i<n; ++i)
-        {
-            ds[i] = i;
+        vector<int> ds(n, 0);
+        for(int i=0; i<n; ++i) ds[i] = i;
+        for(auto pair : pairs) {
+            int fx = find(ds, pair[0]);
+            int fy = find(ds, pair[1]);
+            if(fx != fy) ds[fx] = fy;
         }
-        
-        for(auto pair: pairs)
-        {
-            merge(ds, pair[0],pair[1]);
+        vector<vector<int>> g(n, vector<int>());
+        for(int i=0; i<n; ++i) {
+            int idx = find(ds, i);
+            g[idx].push_back(i);
         }
-        
-        for(int i=0; i<n; ++i)
-        {
-            int j = find(ds, i);
-            str[j].push_back(s[i]);
-        }
-        
-        
-        for(int i=0; i<n; ++i)
-        {
-            if(str[i] != "")
-            {
-                sort(str[i].begin(), str[i].end());
+        for(int i=0; i<n; ++i) {
+            int len = g[i].size();
+            string t = "";
+            for(int j=0; j<len; ++j) {
+                t.push_back(s[g[i][j]]);
+            }
+            sort(t.begin(), t.end());
+            for(int j=0; j<len; ++j) {
+               s[g[i][j]] = t[j]; 
             }
         }
-        
-        int* c = new int[n];
-        memset(c, 0, n*sizeof(int));
-        
-        for(int i=0; i<n; ++i)
-        {
-            int j = find(ds, i);
-            s[i] = str[j][c[j]];
-            ++c[j];
-        }
-        
         return s;
+    }
+private:
+    int find(vector<int> &ds, int x) {
+        return ds[x] == x ? x : ds[x] = find(ds, ds[x]);
     }
 };
