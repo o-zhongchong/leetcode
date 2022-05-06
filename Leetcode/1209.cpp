43b0dc6cc1bc8740e3f472aba4453e361d40e580
o-zhongchong <z_chong@126.com>
2022-05-06 17:46:42 +0800

    feat(1209): Remove All Adjacent Duplicates in String II

--- Leetcode/1209.cpp
+++ Leetcode/1209.cpp
@@ -1,42 +1,19 @@
 class Solution {
 public:
-    string remove(string &s, int k)
-    {
-        int len = s.size();
-        char c;
-        int cnt = 0;
-        
-        for(int i=0; i<len; ++i)
-        {
-            if(cnt == 0 || (cnt != 0 && s[i] != c))
-            {
-                c = s[i];
-                cnt = 1;
-            }
-            else
-            {
-                ++cnt;
-            }
-            
-            if(cnt >= k)
-            {
-                return s.substr(0, i-k+1) + s.substr(i+1, len-i-1);
-            }
-        }
-        
-        return s;
-    }
-    
     string removeDuplicates(string s, int k) {
-        string pre = s;
-        string ret = remove(pre, k);
-        
-        while(ret.size() != pre.size())
-        {
-            pre = ret;
-            ret = remove(ret, k);
+        vector<pair<char,int>> st;
+        for(auto c : s) {
+            if(st.empty() || st.back().first != c) {
+                st.push_back(make_pair(c, 1));
+                continue;
+            }
+            st.back().second += 1;
+            if(st.back().second >= k) st.pop_back();
         }
-        
-        return ret;
+        string ans = "";
+        for(auto t : st) {
+            ans.append(t.second, t.first);
+        }
+        return ans;
     }
 };
