4ebf4a00481d2333f56381fda467feafdb10131a
o-zhongchong <z_chong@126.com>
2022-05-18 18:28:49 +0800

    feat(236): Lowest Common Ancestor of a Binary Tree

--- Leetcode/1192.cpp
+++ Leetcode/1192.cpp
@@ -5,7 +5,6 @@ public:
     {
         vector<vector<int>> g(n);
         for (auto& conn : connections) {
-            sort(conn.begin(), conn.end());
             g[conn[0]].push_back(conn[1]);
             g[conn[1]].push_back(conn[0]);
         }
@@ -15,7 +14,15 @@ public:
         vector<vector<int>> ans;
         dfs(g, 0, -1, cnt, dfn, low);
         for (auto& conn : connections) {
-            if (low[conn[1]] > dfn[conn[0]]) {
+            int u, v;
+            if (dfn[conn[0]] < dfn[conn[1]]) {
+                u = conn[0];
+                v = conn[1];
+            } else {
+                u = conn[1];
+                v = conn[0];
+            }
+            if (low[v] > dfn[u]) {
                 ans.push_back(conn);
             }
         }
@@ -26,7 +33,6 @@ private:
         vector<int>& dfn, vector<int>& low)
     {
         dfn[cur] = low[cur] = cnt++;
-        sort(g[cur].begin(), g[cur].end());
         for (auto& next : g[cur]) {
             if (dfn[next] == 0) {
                 dfs(g, next, cur, cnt, dfn, low);
--- Leetcode/236.cpp
+++ Leetcode/236.cpp
@@ -9,49 +9,10 @@
  */
 class Solution {
 public:
-    TreeNode* helper(TreeNode* root, TreeNode* p, TreeNode* q, int &flag1, int &flag2)
-    {
-        if(root == nullptr){
-            return nullptr;
-        }
-        
-        int flag3, flag4, flag5, flag6;
-        flag3 = flag4 = flag5 = flag6 = 0;
-        
-        TreeNode* left = helper(root->left, p, q, flag3, flag4);
-        
-        if(left != nullptr){
-            return left;
-        }
-        
-        TreeNode* right = helper(root->right, p, q, flag5, flag6);
-        
-        if(right != nullptr){
-            return right;
-        }
-        
-        flag1 = max(flag3, flag5);
-        flag2 = max(flag4, flag6);
-        
-        if(root->val == p->val){
-            flag1 = 1;
-        }
-        
-        if(root->val == q->val)
-        {
-            flag2 = 1;
-        }
-        
-        if(flag1 && flag2){
-            return root;
-        }
-        
-        return nullptr;
-    }
-    
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        int left, right;
-        left = right = 0;
-        return helper(root, p, q, left, right);
+        if(!root || root == p || root == q) return root;
+        TreeNode* L = lowestCommonAncestor(root->left, p, q);
+        TreeNode* R = lowestCommonAncestor(root->right, p, q);
+        return L && R ? root : (L ? L : R);
     }
 };
