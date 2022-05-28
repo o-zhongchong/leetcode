992d8d6248d76c4517f21f9bac2c93b44a083250
o-zhongchong <z_chong@126.com>
2022-05-28 11:57:00 +0800

    feat(268): Missing Number

--- Leetcode/268.cpp
+++ Leetcode/268.cpp
@@ -3,12 +3,9 @@ public:
     int missingNumber(vector<int>& nums) {
         int ret = 0;
         int len = nums.size();
-        
-        for(int i = 0; i < len; ++i)
-        {
+        for (int i = 0; i < len; ++i) {
             ret ^= (i+1) ^ nums[i];
         }
-        
         return ret;
     }
 };
