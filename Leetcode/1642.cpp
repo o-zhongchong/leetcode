7a050d04561a5a973ebf0640011b30d9193d7624
o-zhongchong <z_chong@126.com>
2022-06-21 11:55:19 +0800

    feat(1642): Furthest Building You Can Reach

--- Leetcode/1642.cpp
+++ Leetcode/1642.cpp
@@ -1,46 +1,24 @@
-class Comparator
-{
-public:
-    bool operator()(int &a, int &b)
-    {
-        return a < b;
-    }
-};
-
 class Solution {
 public:
     int furthestBuilding(vector<int>& heights, int bricks, int ladders) {
+        auto cmp = [](const int& a, const int& b) {
+            return a < b;
+        };
         int len = heights.size();
-        priority_queue<int, vector<int>, Comparator> pq;
-        
-        for(int i=1; i<len; ++i)
-        {
-            if(heights[i] <= heights[i-1])
-            {
-                continue;
-            }
-            
+        priority_queue<int, vector<int>, decltype(cmp)> pq(cmp);
+        for (int i=1; i<len; ++i) {
+            if (heights[i] <= heights[i-1]) continue;
             int diff = heights[i] - heights[i-1];
             bricks -= diff;
             pq.push(diff);
-            
-            if(bricks >= 0)
-            {
-                continue;
-            }
-            
-            if(ladders == 0)
-            {
-                return i - 1;
-            }
-            else
-            {
-                bricks += pq.top();
-                pq.pop();
+            if (bricks >= 0) continue;
+            if (ladders <= 0) {
+                return i-1;
+            } else {
+                bricks += pq.top(); pq.pop();
                 --ladders;
             }
         }
-        
-        return len - 1;
+        return len-1;
     }
 };
