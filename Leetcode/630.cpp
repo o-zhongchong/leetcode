0c0bfc373886e508cb57f35d1d27760b16a7df31
o-zhongchong <z_chong@126.com>
2022-06-23 09:31:57 +0800

    feat(630): Course Schedule III

--- Leetcode/630.cpp
+++ Leetcode/630.cpp
@@ -1,37 +1,21 @@
-class Comparator
-{
-public:
-    bool operator()(int &a, int &b)
-    {
-        return a < b;
-    }
-};
-
 class Solution {
 public:
-    static bool cmp(vector<int> &a, vector<int> &b)
-    {
-        return a[1] < b[1];
-    }
-    
     int scheduleCourse(vector<vector<int>>& courses) {
+        auto cmp = [](vector<int> &a, vector<int> &b) {
+            return a[1] < b[1];    
+        };
         int len = courses.size();
-        priority_queue<int, vector<int>, Comparator> pq;
+        priority_queue<int, vector<int>, less<int>> pq;
         sort(courses.begin(), courses.end(), cmp);
         int total = 0;
-        
-        for(int i=0; i<len; ++i)
-        {
+        for (int i=0; i<len; ++i) {
             total += courses[i][0];
             pq.push(courses[i][0]);
-            
-            if(total > courses[i][1])
-            {
+            if (total > courses[i][1]) {
                 total -= pq.top();
                 pq.pop();
             }
         }
-        
         return pq.size();
     }
 };
